3e7c64178b9a9c5b421518cb2db1c6993224e6c6
AbelG101 <abelgebeyehu101@gmail.com>
2022-07-19 10:12:46 -0700

    add the array of project objects to js

--- index.js
+++ index.js
@@ -1,3 +1,86 @@
+const projects = [
+  {
+    name: "Project 1",
+    desription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+  {
+    name: "Project 2",
+    desription:
+      "2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+  {
+    name: "Project 3",
+    desription:
+      "3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+  {
+    name: "Project 4",
+    desription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+  {
+    name: "Project 5",
+    desription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+  {
+    name: "Project 6",
+    desription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi Ut aliquip ex ea commodo consequat.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi",
+    featuredImg: [
+      "images/popup.svg",
+      "images/ic_link.svg",
+      "images/github-logo.svg",
+    ],
+    technologies: ["HTML/CSS", "Ruby on Rails", "JavaScript"],
+    linkLive: "#",
+    linkSource: "#",
+  },
+];
+
+
+
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
